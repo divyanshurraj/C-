7aff29f9dc65351d892034891598b2afada0de61
Divyanshu Raj <101365177+divyanshurraj@users.noreply.github.com>
2023-01-03 17:31:37 +0530

    Create CheckingVowelOrNot.cpp

---
+++ CheckingVowelOrNot.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char c;
+    cout<<"Enter the letter to check it is vowel or not : ";
+    cin>>c;
+    
+    if(c=='a'){
+        cout<<"This is a vowel !";
+    }
+     else if(c=='e'){
+        cout<<"This is a vowel !";
+    }
+     else if(c=='i'){
+        cout<<"This is a vowel !";
+    }
+     else if(c=='o'){
+        cout<<"This is a vowel !";
+    }
+     else if(c=='u'){
+        cout<<"This is a vowel !";
+    }
+    else if(c=='A'){
+        cout<<"This is a vowel !";
+    }
+     else if(c=='E'){
+        cout<<"This is a vowel !";
+    }
+     else if(c=='I'){
+        cout<<"This is a vowel !";
+    }
+    else if(c=='O'){
+        cout<<"This is a vowel !";
+    }
+     else if(c=='U'){
+        cout<<"This is a vowel !";
+    }
+    else{
+        cout<<"This is not a vowel !";
+    }
+    cout<<endl<<"Thanks for using our website !";
+    return 0;
+}
