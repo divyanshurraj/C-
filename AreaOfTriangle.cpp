80b54461b0fa5ea41c0faac5bacc7690951bc127
Divyanshu Raj <101365177+divyanshurraj@users.noreply.github.com>
2023-01-03 17:29:06 +0530

    Create AreaOfTriangle.cpp

---
+++ AreaOfTriangle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float base,height,area;
+    cout<<"Enter the height of triangle. ";
+    cin>>height;
+    cout<<"Enter the base of triangle. ";
+    cin>>base;
+    area=(base*height)*1/2;
+    cout<<"The area of the trangle is : "<<area;
+    return 0;
+
+}
