be08bdfafd173474fff867edf142653cb43ac9ea
Divyanshu Raj <101365177+divyanshurraj@users.noreply.github.com>
2023-01-03 17:27:07 +0530

    Create AreaOfCircle.cpp

---
+++ AreaOfCircle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float pi=3.14;
+    float r,area;
+    cout<<"Enter the radius of circle : ";
+    cin>>r;
+    area=pi*(r*r);
+    cout<<"The area of the circle is : "<<area;
+    return 0;
+}
