017b60e974991674c76121de6447cc5a651d5e3f
Divyanshu Raj <101365177+divyanshurraj@users.noreply.github.com>
2023-01-03 17:30:15 +0530

    Create AsciiValue.cpp

---
+++ AsciiValue.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char c;
+    cout<<"Enter a letter to find its ascii value. ";
+    cin>>c;
+    cout<<"The ascii value of the number is : "<<int(c);
+    return 0;
+}
