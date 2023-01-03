99e7c6810ff412f0f92e1c71ad6e66271ebcfc22
Divyanshu Raj <101365177+divyanshurraj@users.noreply.github.com>
2023-01-03 17:27:48 +0530

    Create AreaOfRectangle.cpp

---
+++ AreaOfRectangle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float length,breadth,area;
+    cout<<"Enter the length : ";
+    cin>>length;
+    cout<<"Enter the breadth : ";
+    cin>>breadth;
+    area=2*(length + breadth);
+    cout<<"The area of rectangle is : "<<area;
+    return 0;
+}
+  
