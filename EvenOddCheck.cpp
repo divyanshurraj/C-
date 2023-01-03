407cd54e09aa04972da5fbbe20978d32cd7dec41
Divyanshu Raj <101365177+divyanshurraj@users.noreply.github.com>
2023-01-03 17:34:21 +0530

    Create EvenOddCheck.cpp

---
+++ EvenOddCheck.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"Enter the number t check even or odd. ";
+    cin>>num;
+    if(num%2==0){
+        cout<<"The number is even. ";
+    }
+    else{
+        cout<<"The number is odd. ";
+    }
+    return 0;
+}
